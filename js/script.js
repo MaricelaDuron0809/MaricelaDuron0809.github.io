23b3b8ed67b90b0a23e9e7da26241b1b783437a9
Maricela Duron <fitcoachmary@gmail.com>
2021-06-24 00:59:26 -0700

    start working on js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-//testing javascript works
-let name = "mary";
-console.log(name);
+//player & computer choice
+let playerChoice = null;
+let computerChoice = null;
+
+//choices-rock-paper-scissor
+const choices = $('.choices')
+console.log(choices);
