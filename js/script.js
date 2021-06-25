ca2736fce5e62b50a977be592660f20ad57ca84a
Maricela Duron <fitcoachmary@gmail.com>
2021-06-25 00:03:15 -0700

    event listener

--- js/script.js
+++ js/script.js
@@ -3,5 +3,10 @@ let playerChoice = null;
 let computerChoice = null;
 
 //choices-rock-paper-scissor
-const choices = $('.choices')
-console.log(choices);
+const $choices = $('.choices')
+console.log($choices);
+
+//event listener for clicked button
+$( ".choices" ).click(function() {
+    //alert( "player choose..." );
+  });
