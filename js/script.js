9200ab185afb4bae9b5b59cf29edd015a3359156
Maricela Duron <fitcoachmary@gmail.com>
2021-06-26 23:30:28 -0700

    add icon images

--- js/script.js
+++ js/script.js
@@ -2,17 +2,32 @@
 let playerChoice = null;
 let computerChoice = null;
 
-//choices-rock-paper-scissor
-const $choices = $('.choices')
-console.log($choices);
+//define choices
+let choices = $('.choices')
+ console.log(choices);
 
 //event listener for clicked button
 $( ".rock" ).click(function() {
-   // alert( "player chose rock!" );
+    playerChoice = "rock";
+   //alert( "player chose rock!" );
+  $('.rock').fadeIn('slow');
   });
   $( ".paper" ).click(function() {
+      playerChoice = "paper";
    // alert( "player chose paper!" );
   });
   $( ".scissor" ).click(function() {
+      playerChoice = "scissor";
    // alert( "player chose scissor!" );
   });
+  
+  //computer random choice
+  //math.random returns num between 0-1
+  //math.floor will round up number
+function compChoice() {
+ let randomNumber = Math.floor(Math.random() * 3)
+ return choices[randomNumber];
+ console.log(compChoice());
+}
+
+  //function
